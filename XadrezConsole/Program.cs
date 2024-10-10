3d71fb2da1f27c2b771a654284a100414362415b
IgorMarcosK <112531270+IgorMarcosK@users.noreply.github.com>
2024-10-10 16:11:20 -0300

    Projeto Criado
    atualizei o sistema criando o projeto

---
+++ XadrezConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tabuleiro;
+
+namespace XadrezC
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            Posicao P;
+            P = new Posicao(3, 4);
+            Console.WriteLine("Posição: " + P);
+
+            Console.ReadLine();
+
+        }
+    }
+}
