3bd4bc2f36378ac2b8cd2e51861685529b1fda24
IgorMarcosK <112531270+IgorMarcosK@users.noreply.github.com>
2024-10-11 18:44:46 -0300

    Peças e tabuleiro (inicio)
    comecei a criar as classes das peças, adicionei as primeiras peças no tabuleiro

--- XadrezConsole/Program.cs
+++ XadrezConsole/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using tabuleiro;
+using XadrezC;
 
 namespace XadrezC
 {
@@ -12,6 +14,11 @@ namespace XadrezC
         static void Main(string[] args)
         {
             Tabuleiro tab = new Tabuleiro(8,8);
+
+            tab.colocarPeca(new Torre(tab, Cor.Preto),new Posicao(0,0));
+            tab.colocarPeca(new Torre(tab, Cor.Preto),new Posicao(1,3));
+            tab.colocarPeca(new Rei(tab, Cor.Preto),new Posicao(2,4));
+
             Tela.imprimirTabuleiro(tab);
             ///System.Console.WriteLine("tabuleiro: "+ tab.linhas);
             Console.ReadLine();
---
+++ XadrezConsole/xadrez/Rei.cs
@@ -0,0 +1,15 @@
+using tabuleiro;
+
+namespace XadrezC{
+    class Rei : Peca
+    {
+        public Rei(Tabuleiro tab, Cor cor) : base(tab, cor)
+        {
+
+        }
+        public override string ToString()
+        {
+            return "R";
+        }
+    }
+}
